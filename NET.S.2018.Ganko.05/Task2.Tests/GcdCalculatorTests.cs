1960349a8508a226a5768473338545b5f5515025
Sergei Ganko <sergei.ganko@gmail.com>
2018-03-24 18:08:44 +0200

    Added GCD finding methods

---
+++ NET.S.2018.Ganko.05/Task2.Tests/GcdCalculatorTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task2.Tests
+{
+    [TestFixture]
+    public class GcdCalculatorTests
+    {
+        #region FindGcdByEuclidTests
+
+        [TestCase(36, 48, ExpectedResult = 12)]
+        [TestCase(48, 36, ExpectedResult = 12)]
+        [TestCase(-36, 48, ExpectedResult = 12)]
+        [TestCase(-36, -48, ExpectedResult = 12)]
+        [TestCase(36, 0, ExpectedResult = 36)]
+        [TestCase(0, 48, ExpectedResult = 48)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        public int FindGcdByEuclid_PassesTwoInts_ExpectsGcdOfThwoInts(int firstNumber, int secondNumber) =>
+            GcdCalculator.FindGcdByEuclid(firstNumber, secondNumber);
+
+        [TestCase(int.MinValue, int.MaxValue)]
+        public void FindGcdByEuclid_PassesTwoInts_ExpectsArgumentException(int firstNumber, int secondNumber) =>
+            Assert.Throws<ArgumentException>((() => GcdCalculator.FindGcdByEuclid(firstNumber, secondNumber)));
+
+        [TestCase(36, 48, 24, ExpectedResult = 12)]
+        [TestCase(-36, 48, -24, ExpectedResult = 12)]
+        [TestCase(36, -48, 24, ExpectedResult = 12)]
+        [TestCase(-36, -48, -24, ExpectedResult = 12)]
+        [TestCase(36, 0, 24, ExpectedResult = 12)]
+        [TestCase(0, 48, 0, ExpectedResult = 48)]
+        public int FindGcdByEuclid_PassesThreeInts_ExpectsGcdOfThreeInts(int firstNumber, int secondNumber, int thirdNumber) =>
+            GcdCalculator.FindGcdByEuclid(firstNumber, secondNumber, thirdNumber);
+
+        [TestCase(12, 6, int.MinValue)]
+        public void FindGcdByEuclid_PassesThreeInts_ExpectsArgumentException(int firstNumber, int secondNumber, int thirdNumber) =>
+            Assert.Throws<ArgumentException>((() => GcdCalculator.FindGcdByEuclid(firstNumber, secondNumber, thirdNumber)));
+
+        [TestCase(36, 48, 26, 52, ExpectedResult = 2)]
+        [TestCase(36, 48, 24, 60, ExpectedResult = 12)]
+        [TestCase(0, 2, 0, 4, ExpectedResult = 2)]
+        public int FindGcdByEuclid_PassesFourInts_ExpectsGcdOfFourInts(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) =>
+            GcdCalculator.FindGcdByEuclid(firstNumber, secondNumber, thirdNumber, fourthNumber);
+
+        [TestCase(12, 6, int.MinValue, 0)]
+        public void FindGcdByEuclid_PassesFourInts_ExpectsArgumentException(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) =>
+            Assert.Throws<ArgumentException>((() => GcdCalculator.FindGcdByEuclid(firstNumber, secondNumber, thirdNumber, fourthNumber)));
+
+        [TestCase(36, 48, 24, 60, 84, 72, ExpectedResult = 12)]
+        [TestCase(36, -48, 24, 60, -84, 72, ExpectedResult = 12)]
+        [TestCase(36, 48, 0, 60, 84, 0, ExpectedResult = 12)]
+        public int FindGcsByEuclid_PassesParamsInt_ExpectsGcd(params int[] numbers) =>
+            GcdCalculator.FindGcdByEuclid(numbers);
+
+        [TestCase(24, 48, 12, 0, int.MinValue, 36)]
+        public void FindGcsByEuclid_PassesParamsInt_ExpectsArgumentException(params int[] numbers) =>
+            Assert.Throws<ArgumentException>((() => GcdCalculator.FindGcdByEuclid(numbers)));
+
+        #endregion
+
+        #region FindGcdBySteinTests
+
+        [TestCase(116150, 232704, ExpectedResult = 202)]
+        [TestCase(0, 232704, ExpectedResult = 232704)]
+        [TestCase(116150, 0, ExpectedResult = 116150)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(116150, -232704, ExpectedResult = 202)]
+        [TestCase(-116150, -232704, ExpectedResult = 202)]
+        public int FindGcdByStein_PassesTwoInts_ExpectsGcd(int firstNumber, int secondNumber) =>
+            GcdCalculator.FindGcdByStein(firstNumber, secondNumber);
+
+        [TestCase(202, int.MinValue)]
+        public void FindGcdByStein_PassesTwoInts_ExpectsArgumentException(int firstNumber, int secondNumber) =>
+            Assert.Throws<ArgumentException>(() => GcdCalculator.FindGcdByStein(firstNumber, secondNumber));
+
+        [TestCase(116150, 232704, 404, ExpectedResult = 202)]
+        public int FindGcdByStein_PassesThreeInts_ExpectsGcd(int firstNumber, int secondNumber, int thirdNumber) =>
+            GcdCalculator.FindGcdByStein(firstNumber, secondNumber, thirdNumber);
+
+        [TestCase(202, 808, int.MinValue)]
+        public void FindGcdByStein_PassesThreeInts_ExpectsArgumentException(int firstNumber, int secondNumber, int thirdNumber) =>
+            Assert.Throws<ArgumentException>(() => GcdCalculator.FindGcdByStein(firstNumber, secondNumber, thirdNumber));
+
+        [TestCase(116150, 232704, 404, 808, ExpectedResult = 202)]
+        public int FindGcdByStein_PassesFourInts_ExpectsGcd(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) =>
+            GcdCalculator.FindGcdByStein(firstNumber, secondNumber, thirdNumber, fourthNumber);
+
+        [TestCase(202, int.MinValue, 808, 404)]
+        public void FindGcdByStein_PassesFourInts_ExpectsArgumentException(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber) =>
+            Assert.Throws<ArgumentException>(() => GcdCalculator.FindGcdByStein(firstNumber, secondNumber, thirdNumber, fourthNumber));
+
+        [TestCase(116150, 232704, 808, 1010, 404, ExpectedResult = 202)]
+        public int FindGcdByStein_PassesParamsInts_ExpectsGcd(params int[] numbers) =>
+            GcdCalculator.FindGcdByStein(numbers);
+
+        #endregion
+    }
+}
---
+++ NET.S.2018.Ganko.05/Task2/GcdCalculator.cs
@@ -0,0 +1,233 @@
+﻿using System;
+
+namespace Task2
+{
+    /// <summary>
+    /// Class encapsulates GCD calculation methods
+    /// </summary>
+    public static class GcdCalculator
+    {
+        #region FindGcdByEuclid
+
+        /// <summary>
+        /// Finds the GCD by euclid.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <returns>Returns GCD of two numbers</returns>
+        /// <exception cref="ArgumentException">Throws when argument value is int.MinValue</exception>
+        public static int FindGcdByEuclid(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == int.MinValue || secondNumber == int.MinValue)
+            {
+                throw new ArgumentException($"Can't be calculated, because |{int.MinValue}| is out of range of int");
+            }
+
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber < 0)
+            {
+                firstNumber = Math.Abs(firstNumber);
+            }
+
+            if (secondNumber < 0)
+            {
+                secondNumber = Math.Abs(secondNumber);
+            }
+
+            while (secondNumber != 0)
+            {
+                firstNumber %= secondNumber;
+                Swap(ref firstNumber, ref secondNumber);
+            }
+
+            return firstNumber;
+        }
+
+        /// <summary>
+        /// Finds the GCD by euclid.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <param name="thirdNumber">The third number.</param>
+        /// <returns>Returns GCD of three numbers</returns>
+        public static int FindGcdByEuclid(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return FindGcdByEuclid(FindGcdByEuclid(firstNumber, secondNumber), thirdNumber);
+        }
+
+        /// <summary>
+        /// Finds the GCD by euclid.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <param name="thirdNumber">The third number.</param>
+        /// <param name="fourthNumber">The fourth number.</param>
+        /// <returns>Returns GCD of four numbers</returns>
+        public static int FindGcdByEuclid(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber)
+        {
+            int gcdOfFirstAndSecond = FindGcdByEuclid(firstNumber, secondNumber);
+            int gcdOfThirdAndFourth = FindGcdByEuclid(thirdNumber, fourthNumber);
+            return FindGcdByEuclid(gcdOfFirstAndSecond, gcdOfThirdAndFourth);
+        }
+
+        /// <summary>
+        /// Finds the GCD by euclid.
+        /// </summary>
+        /// <param name="numbers">The numbers.</param>
+        /// <returns>Returns GCD of numbers</returns>
+        public static int FindGcdByEuclid(params int[] numbers)
+        {
+            int gcd = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                gcd = FindGcdByEuclid(gcd, numbers[i]);
+            }
+
+            return gcd;
+        }
+
+        #endregion
+
+        #region FindGcdByStein
+
+        /// <summary>
+        /// Finds the GCD by stein.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <returns>Returns GCD of two numbers</returns>
+        /// <exception cref="ArgumentException">Throws when argument value is int.MinValue</exception>
+        public static int FindGcdByStein(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == int.MinValue || secondNumber == int.MinValue)
+            {
+                throw new ArgumentException($"Can't be calculated, because |{int.MinValue}| is out of int range");
+            }
+
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber < 0)
+            {
+                firstNumber = Math.Abs(firstNumber);
+            }
+
+            if (secondNumber < 0)
+            {
+                secondNumber = Math.Abs(secondNumber);
+            }
+
+            bool isFirstNumberEven = (firstNumber & 1) == 0;
+            bool isSecondNumberEven = (secondNumber & 1) == 0;
+
+            if (isFirstNumberEven && isSecondNumberEven)
+            {
+                return FindGcdByStein(firstNumber >> 1, secondNumber >> 1) << 1;
+            }
+
+            if (isFirstNumberEven && !isSecondNumberEven)
+            {
+                return FindGcdByStein(firstNumber >> 1, secondNumber);
+            }
+
+            if (isSecondNumberEven)
+            {
+                return FindGcdByStein(firstNumber, secondNumber >> 1);
+            }
+
+            if (firstNumber > secondNumber)
+            {
+                return FindGcdByEuclid((firstNumber - secondNumber) >> 1, secondNumber);
+            }
+
+            return FindGcdByStein(firstNumber, (secondNumber - firstNumber) >> 1);
+        }
+
+        /// <summary>
+        /// Finds the GCD by stein.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <param name="thirdNumber">The third number.</param>
+        /// <returns>Returns GCD of three numbers</returns>
+        public static int FindGcdByStein(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return FindGcdByStein(FindGcdByStein(firstNumber, secondNumber), thirdNumber);
+        }
+
+        /// <summary>
+        /// Finds the GCD by stein.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        /// <param name="thirdNumber">The third number.</param>
+        /// <param name="fourthNumber">The fourth number.</param>
+        /// <returns>Returns GCD of four numbers</returns>
+        public static int FindGcdByStein(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber)
+        {
+            return FindGcdByStein(FindGcdByStein(firstNumber, secondNumber), FindGcdByStein(thirdNumber, fourthNumber));
+        }
+
+        /// <summary>
+        /// Finds the GCD by stein.
+        /// </summary>
+        /// <param name="numbers">The numbers.</param>
+        /// <returns>Returns GCD of numbers</returns>
+        public static int FindGcdByStein(params int[] numbers)
+        {
+            int gcd = numbers[0];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                gcd = FindGcdByStein(gcd, numbers[i]);
+            }
+
+            return gcd;
+        }
+
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// Swaps two numbers.
+        /// </summary>
+        /// <param name="firstNumber">The first number.</param>
+        /// <param name="secondNumber">The second number.</param>
+        private static void Swap(ref int firstNumber, ref int secondNumber)
+        {
+            int temp = secondNumber;
+            secondNumber = firstNumber;
+            firstNumber = temp;
+        }
+
+        #endregion
+    }
+}
